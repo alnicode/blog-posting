14ab5b34435711496734b5f64a810a14b066ca04
alnicode32 <albensnico@gmail.com>
2022-03-11 03:01:08 -0500

    Inicio del proyecto

---
+++ src/main/java/com/alnicode/blogposting/BlogPostingApplication.java
@@ -0,0 +1,13 @@
+package com.alnicode.blogposting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogPostingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogPostingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alnicode/blogposting/BlogPostingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alnicode.blogposting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogPostingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
